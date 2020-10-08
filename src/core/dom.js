2c1692ef8fe4002207d08c842e8db896ec5f2df5
Vitaliy <michael8913@gmail.com>
2020-10-08 09:14:39 +0300

    selected group of cells

--- src/core/dom.js
+++ src/core/dom.js
@@ -67,6 +67,18 @@ class Dom {
     removeClass(className) {
         this.$el.classList.remove(className);
     }
+
+    id(parse) {
+        if (parse) {
+            const parsed = this.id().split(":");
+            return {
+                row: +parsed[0],
+                col: +parsed[1],
+            };
+        } else {
+            return this.data.id;
+        }
+    }
 }
 
 export function $(selector) {
