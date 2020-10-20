e595f572d0d3adccde765839d66075f304869b2f
Vitaliy Inshakov <michael8913@gmail.com>
2020-10-20 09:09:50 +0300

    render state

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,6 +3,8 @@ const CODES = {
     Z: 90,
 };
 
+const DEFAULT_WIDTH = 120;
+
 function createRow(index, content) {
     return `
         <div class="row" data-type="resizable">
@@ -15,16 +17,16 @@ function createRow(index, content) {
     `;
 }
 
-function createColumn(column, index) {
+function createColumn({ column, index, width }) {
     return `
-        <div class="column" data-type="resizable" data-col="${index}">
+        <div class="column" data-type="resizable" data-col="${index}" style="width: ${width}">
             ${column}
             <div class="col-resize" data-resize="col"/>
         </div>
     `;
 }
 
-function createCell(row) {
+function createCell(state, row) {
     return function(_, col) {
         return `
             <div
@@ -33,6 +35,7 @@ function createCell(row) {
                 data-col="${col}"
                 data-id="${row}:${col}"
                 data-type="cell"
+                style="width: ${getWidth(state.colState, col)}"
             />
         `;
     };
@@ -42,19 +45,32 @@ function toChar(_, index) {
     return String.fromCharCode(CODES.A + index);
 }
 
-export function createTable(rowsCount = 15) {
+function getWidth(state, index) {
+    return (state[index] || DEFAULT_WIDTH) + "px";
+}
+
+function withWidthFromState(state) {
+    return function(col, index) {
+        return {
+            col, index, width: getWidth(state.colState, index),
+        };
+    };
+}
+
+export function createTable(rowsCount = 15, state = {}) {
     const colsCount = CODES.Z - CODES.A + 1;
     const rows = [];
     const columns = new Array(colsCount)
         .fill("")
         .map(toChar)
+        .map(withWidthFromState(state))
         .map(createColumn)
         .join("");
 
     rows.push(createRow(null, columns));
 
     for (let row = 0; row < rowsCount; row++) {
-        const cells = new Array(colsCount).fill("").map(createCell(row)).join("");
+        const cells = new Array(colsCount).fill("").map(createCell(state, row)).join("");
         rows.push(createRow(row + 1, cells));
     }
 
