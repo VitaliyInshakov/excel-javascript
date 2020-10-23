1f98c18081efb68b2619cc4a6d89811efa8fe481
Vitaliy Inshakov <michael8913@gmail.com>
2020-10-23 08:45:29 +0300

    added debounce

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ import { Formula } from "@/components/formula/Formula";
 import { Table } from "@/components/table/Table";
 import { createStore } from "@core/createStore";
 import { rootReducer } from "@/store/rootReducer";
-import { storage } from "@core/utils";
+import { debounce, storage } from "@core/utils";
 import { initialState } from "@/store/initialState";
 import "./styles/index.scss";
 
 const store = createStore(rootReducer, initialState);
 
-store.subscribe(state => {
-    storage("excel-state", state);
-});
+const stateListener = debounce(state => storage("excel-state", state), 300);
+store.subscribe(stateListener);
 
 const excel = new Excel("#app", {
     components: [Header, Toolbar, Formula, Table],
